dfa7a90f9d0a16b793d8d4b4d64c45faa51fc6b9
Jaider <lab@labinfo.is.escuelaing.edu.co>
2024-08-29 15:56:05 -0500

    crear clases de prueba

---
+++ src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -0,0 +1,14 @@
+package edu.eci.cvds.tdd.library;
+
+import org.junit.jupiter.api.Test;
+
+public class LibraryTest {
+    public LibraryTest(){
+
+    }
+    @Test
+    public void shouldExistsBook(){
+
+    }
+
+}
