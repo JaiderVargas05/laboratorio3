18c70c906a424d604a3ccb73721f84932194309a
Jaider <jaider.vargas-n@mail.escuelaing.edu.co>
2024-08-29 20:14:29 -0500

    preparar ambiente de pruebas

--- src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -1,14 +1,23 @@
 package edu.eci.cvds.tdd.library;
+import static org.junit.jupiter.api.Assertions.*;
 
+import edu.eci.cvds.tdd.library.book.Book;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.BeforeEach;
 public class LibraryTest {
-    public LibraryTest(){
-
+    private Library library;
+    private Book book;
+    @BeforeEach
+    public void init(){
+        library = new Library();
     }
     @Test
     public void shouldExistsBook(){
-
+        assertTrue(library.getBooks().containsKey(book.getIsbn()));
+    }
+    @Test
+    public void shouldNotExistsBook(){
+        assertTrue(!library.getBooks().containsKey(book.getIsbn()));
     }
 
 }
